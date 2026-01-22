8ff6acaf8d4c5b5e82430ab7b0849127b75da61e
nikolpetkova10 <petkovaniki123@gmail.com>
2026-01-22 13:11:02 +0200

    Add button and warning for grade input

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 name = st.text_input(" Vavedete tekst.")
 grade = st.number_input("Vuvedete ocenka.")
+st.button(Proverka)
+if ( grade > 6):
+  st.warning("Ocenkata e po golqma ot 6")
+  
